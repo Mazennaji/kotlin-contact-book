32ca30770a0f4c0c3dd2a4272305fe42717c064c
Mazennaji <mazen.naji.1@gmail.com>
2026-09-11 14:37:45 +0300

    Add details() function using string templates and multiline strings

--- src/Contact.kt
+++ src/Contact.kt
@@ -2,4 +2,10 @@ class Contact(
     val name: String,
     var phone: String,
     var email: String? = null
-)
+) {
+    fun details(): String = """
+        Name : $name
+        Phone: $phone
+        Email: ${email ?: "N/A"}
+    """.trimIndent()
+}
