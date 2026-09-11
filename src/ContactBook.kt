8a0862287e4a9c53133ea260ce5ea61ee57e2255
Mazennaji <mazen.naji.1@gmail.com>
2026-09-11 22:07:12 +0300

    Implement add() with duplicate check

--- src/ContactBook.kt
+++ src/ContactBook.kt
@@ -1,3 +1,12 @@
 class ContactBook {
-    private val contacts = mutableListOf<Contact>()
+    private val contacts = mutableListOf<Contact>()   // mutable list of contacts
+
+    fun add(contact: Contact) {
+        if (contacts.contains(contact)) {
+            println("Contact with phone ${contact.phone} already exists.")
+        } else {
+            contacts.add(contact)
+            println("Added: $contact")
+        }
+    }
 }
